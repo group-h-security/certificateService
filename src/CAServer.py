4e9a527f081c4067eed9603a6fb11b81d184e5a1
Ryan Sheridan <0xryann@protonmail.com>
2025-11-12 11:38:49 +0000

    fix: for local, docker broke without 0.0.0.0, TODO

--- src/CAServer.py
+++ src/CAServer.py
@@ -172,5 +172,5 @@ def printCert(csr):
     return cert_pem
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
+    app.run(host="127.0.0.1", port=5000, debug=True, use_reloader=False)
 
