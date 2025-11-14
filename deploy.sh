678656f10c4e4888bf003163f87558498b95cc1f
Ryan Sheridan <0xryann@protonmail.com>
2025-11-14 23:16:04 +0000

    final

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
+set -e
+
+# build and push
 docker buildx build --platform linux/amd64 -t caserver:latest --load .
 docker tag caserver:latest ghcr.io/group-h-security/caserver:latest
 docker push ghcr.io/group-h-security/caserver:latest
+
+# pull latest image
+docker pull ghcr.io/group-h-security/caserver:latest
+
+# start docker compose for caddy
+docker compose down
+docker compose up -d --build
 
